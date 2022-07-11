0d7cf09ccfe5ad44b68461073a21e8c49601bcd1
dcmanproductions <dcmanproductions@gmail.com>
2022-07-11 18:57:27 -0400

    Added Converter Class

---
+++ Convert.cs
@@ -0,0 +1,40 @@
+﻿namespace CLMath;
+
+public static class Convert
+{
+    public static int ConvertToInt32(string value)
+    {
+        if (string.IsNullOrEmpty(value) || !int.TryParse(value, out int result))
+        {
+            return 0;
+        }
+        return result;
+    }
+
+    public static long ConvertToInt64(string value)
+    {
+        if (string.IsNullOrEmpty(value) || !long.TryParse(value, out long result))
+        {
+            return 0;
+        }
+        return result;
+    }
+
+    public static float ConvertToReal32(string value)
+    {
+        if (string.IsNullOrEmpty(value) || !float.TryParse(value, out float result))
+        {
+            return 0;
+        }
+        return result;
+    }
+
+    public static double ConvertToReal64(string value)
+    {
+        if (string.IsNullOrEmpty(value) || !double.TryParse(value, out double result))
+        {
+            return 0;
+        }
+        return result;
+    }
+}
