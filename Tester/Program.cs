06f92a1e6f09dabaa9a11698f3db349320e6edb2
dcmanproductions <dcmanproductions@gmail.com>
2022-07-15 20:53:35 -0400

    Added tester

---
+++ Tester/Program.cs
@@ -0,0 +1,8 @@
+﻿using ChaseLabs.Math;
+
+string shared = "Hello/World64.jpg";
+string unenc = "Hello World";
+string crypt = AESMath.EncryptStringAES(unenc, shared);
+Console.WriteLine(unenc);
+Console.WriteLine(crypt);
+Console.WriteLine(AESMath.DecryptStringAES(crypt, shared));
