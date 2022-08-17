0b0eed43d852511e6c14d9e412b01939142d12a9
dcmanproductions <dcmanproductions@gmail.com>
2022-08-16 22:06:11 -0400

    Fixed bits to bytes

--- CLMath/FileMath.cs
+++ CLMath/FileMath.cs
@@ -8,41 +8,62 @@ namespace CLMath;
 /// </summary>
 public class CLFileMath
 {
+    public static (string[] added, string[] removed, string[] changed) FileDiffChecker(string older, string newer)
+    {
+        if (Directory.Exists(older) && Directory.Exists(newer))
+        {
+            List<string> added, removed, changed;
+            added = removed = changed = new();
+
+            string[] files_in_older = Directory.GetFiles(older, "*.*", SearchOption.AllDirectories);
+            string[] files_in_newer = Directory.GetFiles(newer, "*.*", SearchOption.AllDirectories);
+            Parallel.ForEach(files_in_older, file =>
+            {
+                string relative = Path.GetRelativePath(older, file);
+
+            });
+
+            return (added.ToArray(), removed.ToArray(), changed.ToArray());
+        }
+
+        return (Array.Empty<string>(), Array.Empty<string>(), Array.Empty<string>());
+    }
+
     #region Public Methods
 
     public static string AdjustedFileSize(double bytes)
     {
         if (Round(double.Parse("" + bytes, NumberStyles.Any, CultureInfo.InvariantCulture), 2) < 1024)
         {
-            return Round(double.Parse("" + bytes, NumberStyles.Any, CultureInfo.InvariantCulture), 2) + "b";
+            return Round(double.Parse("" + bytes, NumberStyles.Any, CultureInfo.InvariantCulture), 2) + "B";
         }
         else if (Round(FileSizeKB(bytes), 2) < 1024)
         {
-            return Round(FileSizeKB(bytes), 2) + "Kb";
+            return Round(FileSizeKB(bytes), 2) + "KB";
         }
         else if (Round(FileSizeMB(bytes), 2) < 1024)
         {
-            return Round(FileSizeMB(bytes), 2) + "Mb";
+            return Round(FileSizeMB(bytes), 2) + "MB";
         }
         else if (Round(FileSizeGB(bytes), 2) < 1024)
         {
-            return Round(FileSizeGB(bytes), 2) + "Gb";
+            return Round(FileSizeGB(bytes), 2) + "GB";
         }
         else if (Round(FileSizeTB(bytes), 2) < 1024)
         {
-            return Round(FileSizeTB(bytes), 2) + "Tb";
+            return Round(FileSizeTB(bytes), 2) + "TB";
         }
         else if (Round(FileSizePB(bytes), 2) < 1024)
         {
-            return Round(FileSizePB(bytes), 2) + "Pb";
+            return Round(FileSizePB(bytes), 2) + "PB";
         }
         else if (Round(FileSizeEB(bytes), 2) < 1024)
         {
-            return Round(FileSizeEB(bytes), 2) + "Eb";
+            return Round(FileSizeEB(bytes), 2) + "EB";
         }
         else
         {
-            return Round(FileSizeZB(bytes), 2) + "Zb";
+            return Round(FileSizeZB(bytes), 2) + "ZB";
         }
     }
 
