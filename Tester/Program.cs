23400d397e2471c2215ba2134fab557429a13a80
Drew Chase <dcmanproductions@gmail.com>
2023-02-10 10:33:58 -0500

    Added File Math

--- Tester/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using CLMath;
-
-string plainText = "Hello World";
-string base64 = CLConverter.EncodeBase64(plainText);
-Console.WriteLine(plainText);
-Console.WriteLine(base64);
-Console.WriteLine(CLConverter.DecodeBase64(base64));
